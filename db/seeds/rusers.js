81e923a1be03bd45e30a2d5339a82ccd7eb434aa
Michael Stranges <strangesm@gmail.com>
2018-04-06 21:00:45 +0000

    added cookies, enabled registration, and and active user profile page that pull data from db

--- db/seeds/rusers.js
+++ db/seeds/rusers.js
@@ -5,16 +5,16 @@ exports.seed = function(knex, Promise) {
   return knex('rusers').del()
     .then(function () {
       return Promise.all([
-        knex('rusers').insert({id: 1, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 1}),
-        knex('rusers').insert({id: 2, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 2}),
-        knex('rusers').insert({id: 3, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 3}),
-        knex('rusers').insert({id: 4, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 4}),
-        knex('rusers').insert({id: 5, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 5}),
-        knex('rusers').insert({id: 6, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 6}),
-        knex('rusers').insert({id: 7, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 7}),
-        knex('rusers').insert({id: 8, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 8}),
-        knex('rusers').insert({id: 9, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 9}),
-        knex('rusers').insert({id: 10, password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 10})
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 1}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 2}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 3}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 4}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 5}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 6}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 7}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 8}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 9}),
+        knex('rusers').insert({ password: 'password', handle: chance.twitter().slice(1), email: chance.email(), user_info_id: 10})
       ]);
     });
 };
--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const app         = express();
+const PORT         = process.env.PORT || 8080;
+const ENV          = process.env.ENV || "development";
+const express      = require("express");
+const bodyParser   = require("body-parser");
+const sass         = require("node-sass-middleware");
+const app          = express();
+const cookieParser = require('cookie-parser') //cookie parser added
 
 const knexConfig  = require("./knexfile");
 
@@ -21,6 +22,8 @@ const routes = require("./routes/routes");
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(cookieParser()); // cookie parser added
+
 app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
