8daa96015ad9bb8f786abbfb0f4c4f5d51388bd7
Thomas Saunders <thomas130@fastmail.com>
2018-04-06 04:04:52 +0000

    freezing db_access before experimental branch

--- db/seeds/resources.js
+++ db/seeds/resources.js
@@ -1,6 +1,6 @@
 var ogParser = require("og-parser");
 
-seedResourceFromUrl = function (knex, url, creator, subject) {
+seedResourceFromUrl = function (knex, id, url, creator, subject) {
   return new Promise(function (resolve, reject) {
     ogParser(url, function (error, data) {
       let imgurl = data.og.image.url;
@@ -8,11 +8,13 @@ seedResourceFromUrl = function (knex, url, creator, subject) {
         imgurl = data.og.url.substr(0, data.og.url.slice(8).search("/")+8) + imgurl;
       }
       knex('resources').insert({
+        id: id,
         url: url,
         title: data.og.title,
         description: data.og.description,
         image_url: imgurl,
-        creator_id: creator
+        creator_id: creator,
+        subject_id: subject
       }).then(resolve).catch(reject);
     });
   })
@@ -22,8 +24,16 @@ exports.seed = function (knex, Promise) {
   return knex('resources').del()
     .then(function () {
       return Promise.all([
-        seedResourceFromUrl(knex, 'https://www.sciencedaily.com/releases/2018/04/180405093238.htm', 4, 7),
-        seedResourceFromUrl(knex, 'https://www.usmagazine.com/entertainment/news/roseanne-slammed-for-mocking-fresh-off-the-boat-black-ish/', 5, 2)
+        seedResourceFromUrl(knex, 1, 'https://www.sciencedaily.com/releases/2018/04/180405093238.htm', 4, 7),
+        seedResourceFromUrl(knex, 2, 'https://www.usmagazine.com/entertainment/news/roseanne-slammed-for-mocking-fresh-off-the-boat-black-ish/', 5, 2),
+        seedResourceFromUrl(knex, 3, 'https://www.sciencedaily.com/releases/2018/04/180405093238.htm', 4, 7),
+        seedResourceFromUrl(knex, 4, 'https://www.usmagazine.com/entertainment/news/roseanne-slammed-for-mocking-fresh-off-the-boat-black-ish/', 5, 2),
+        seedResourceFromUrl(knex, 5, 'https://www.sciencedaily.com/releases/2018/04/180405093238.htm', 4, 7),
+        seedResourceFromUrl(knex, 6, 'https://www.usmagazine.com/entertainment/news/roseanne-slammed-for-mocking-fresh-off-the-boat-black-ish/', 5, 2),
+        seedResourceFromUrl(knex, 7, 'https://www.sciencedaily.com/releases/2018/04/180405093238.htm', 4, 7),
+        seedResourceFromUrl(knex, 8, 'https://www.usmagazine.com/entertainment/news/roseanne-slammed-for-mocking-fresh-off-the-boat-black-ish/', 5, 2),
+        seedResourceFromUrl(knex, 9, 'https://www.sciencedaily.com/releases/2018/04/180405093238.htm', 4, 7),
+        seedResourceFromUrl(knex, 10, 'https://www.usmagazine.com/entertainment/news/roseanne-slammed-for-mocking-fresh-off-the-boat-black-ish/', 5, 2)
       ]);
 });
 };
