b8ba5a90593a22b4f623d17e354d842ee81d942e
Jojo <jody.sekhon@gmail.com>
2018-04-06 00:46:47 +0000

    Added a createArticles function and render articles function. To populate home.ejs through jquery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,32 +3,173 @@ $(() => {
     method: "GET",
     url: "/api/users"
   }).done((users) => {
-    for(user of users) {
+    for (user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
 });
 
 
-const articleData = {
-  "user": {
-    "name": "Newton",
-    },
- "resourceURL": {
-    "url": "https://www.google.ca/"
-  },
-"title": "Hello This is a Title",
-  "Description": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-"created_at": 1461116232227,
-"imageURL": "https://images.unsplash.com/photo-1502787530428-11cf61d6ba18?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ee60964c06a30ae7596dce9f7380a391&auto=format&fit=crop&w=800&q=60"
+const article = [
 
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  },
+
+  {
+    "user": "jody",
+    "title": "Science is Great",
+    "imageUrl": "https://images.unsplash.com/photo-1497197952040-45d5388447e9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f88666296af0342fcd67258d6e9531df&auto=format&fit=crop&w=800&q=60",
+
+    "articleUrl": "https://www.sciencedaily.com/releases/2018/04/180404133532.htm",
+    "description": "This article is amazing. It's about absolutely nothing",
+    "likes": 5,
+    "averageRating": 4,
+    "userRating": 4
+
+  }
+
+
+
+
+]
+
+
+
+
+function createArticleElement(article) {
+  let user = article.user;
+  let title = article.title;
+  let url = article.articleUrl;
+  let image = article.imageUrl;
+  let des = article.description;
+  let heartClasses = article.likes;
+  let average = article.averageRating;
+  let ratingClass = article.userRating;
+  console.log(article);
+  const articleHTML =
+
+    `<div class = "col-sm">
+<div class="card" style="width: 28rem;">
+  <a href="${url}"><img class="card-img-top" src="${image}"> </a>
+  <div class="card-body">
+    <h3>${title}</h3>
+    <p class="card-text">${des}</p>
+  </div>
+ <footer class = "card-footer">
+<div class="icons">
+<i class="fas fa-heart ${heartClasses}"></i>
+   <div class="rating">
+   <i class="far fa-star ${ratingClass}"></i>
+   <i class="far fa-star"></i>
+   <i class="far fa-star"></i>
+   <i class="far fa-star"></i>
+   <i class="far fa-star"></i>
+<p>Rating: ${average}</p>
+ </div>
+  </div>
+ </footer>
+</div>`
+
+  return articleHTML;
 }
 
-
-
-
+function renderArticles(articles) {
+  var row = "";
+  for (var i = 0; i < articles.length; i++) {
+    if (i % 4 === 0) {
+      row = $("<div/>").addClass("row");
+    }
+    row.prepend(createArticleElement(articles[i]));
+    if (i % 4 === 3) {
+      $(".article-container").prepend(row);
+    }
+  }
+  if (articles.length % 4 !== 0) {
+    $(".article-container").prepend(row)
+  }
+}
 
 
 jQuery(document).ready(function($) {
@@ -44,44 +185,11 @@ jQuery(document).ready(function($) {
     $(tab_content).addClass('active');
   });
 
-function createArticleElement(article) {
-  let user =  article.user.name;
-  let resourceURL = article.resourceURL.url;
-  let title = article.title;
-  let description = article.description.text;
-  let dateCreated = article.created_at;
-  let heartClasses = article.liked ? 'liked' : '';
-
-  const resourceHTML =
-
-`<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="${imgURl}" alt="Card image cap">
-    <div class="card-body">
-    <h5 class="card-title">${title}</h5>
-    <p class="card-text"> ${description} </p>
-    </div>
-  <footer>
-<div>
-<p class="date">${dateCreated}</p>
-</div>
-<div class="icons">
-<i class="fas fa-heart ${heartClasses}"></i>
-   </div>
- </footer>
-</div>
-</div>`
-
-  return resourceHTML;
-}
 
 
-
-
-
-
+  renderArticles(article);
 
 });
-
 
 
 
