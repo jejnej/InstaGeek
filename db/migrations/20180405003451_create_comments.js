7948d2b14eec29c88df51747057ee547db659c47
saunderst <thomas130@gmx.com>
2018-04-07 22:58:11 -0400

    ability to post comments on server

--- db/migrations/20180405003451_create_comments.js
+++ db/migrations/20180405003451_create_comments.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('comments', function (table) {
+    table.increments('id');
     table.string('comment_text');
     table.integer('resource_id');
     table.integer('user_id');
-    table.primary(['resource_id', 'user_id']);
   });
 };
 
